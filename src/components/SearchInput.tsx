b646b2f46f42fa338aaecac3acd361a3c7932afd
MarcinMN <79340820+MarcinMN@users.noreply.github.com>
2023-05-02 21:49:33 -0500

    Add comments to components V

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,14 +1,25 @@
+/* Search Input box for searching games by title. */
+
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
+/* Prop is a function that takes the searchText from the search
+field. */
 interface Props {
   onSearch: (searchText: string) => void;
 }
 
+/* Ref hook used since this is a simple form with one input. References an 
+HTMLInputElement. Note that ref is references in the Input tag below. */
 const SearchInput = ({ onSearch }: Props) => {
   const ref = useRef<HTMLInputElement>(null);
 
+  /* InputGroup is used to include the BsSearch icon (InputLeftElement) 
+  in the search field. Form element works using an event via onSubmit.
+  preventDefault keeps the form from being posted to the server. Form width
+  was set in index.css. When a search term is submitted, the text is sent to
+  onSearch, which is used to generate the SearchInput  */
   return (
     <form
       onSubmit={(event) => {
